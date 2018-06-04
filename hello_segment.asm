9135e90bfef6f09d8aebe2787adda326bf4fa6e2
tor4z <vwenjie@hotmail.com>
2018-06-05 03:21:21 +0800

    add comment

--- hello_segment.asm
+++ hello_segment.asm
@@ -1,3 +1,18 @@
+; A segmented memory model divides the system memory into groups of independent segments, referenced by
+; pointers located in the segment registers. Each segment is used to contain a specific type of data. One segment
+; is used to contain instruction codes, another segment stores the data elements, and a third segment keeps the
+; program stack.
+
+; *Data segment - it is represented by .data section and the .bss. The .data section is used to declare the
+;  memory region where data elements are stored for the program. This section cannot be expanded after the
+;  data elements are declared, and it remains static throughout the program.
+;  The .bss section is also a static memory section that contains buffers for data to be declared later in the
+;  program. This buffer memory is zero-filled.
+
+; *Code segment - it is represented by .text section. This defines an area in memory that stores the instruction
+;  codes. This is also a fixed area.
+; *Stack - this segment contains data values passed to functions and procedures within the program.
+
 segment .text
 	global main
 main:
